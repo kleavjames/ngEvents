1bffb020460a318597b2e5cbb69f3b4d6b13c11c
kleavjames <kleavantjames@gmail.com>
2018-08-18 01:42:21 +0800

    exploring the angular template syntax (section 4)

---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-events-list',
+  template: `
+  <div>
+    <h1>Upcoming Angular Events</h1>
+    <hr>
+    <app-event-thumbnail [event]="firstEvent"></app-event-thumbnail>
+  </div>
+  `
+})
+
+export class EventsListComponent implements OnInit {
+  firstEvent = {
+      id: 1,
+      name: 'Angular Connect',
+      date: '9/26/2036',
+      time: '10:00 am',
+      price: 599.99,
+      imageUrl: '/assets/images/angularconnect-shield.png',
+      location: {
+        address: '1057 DT',
+        city: 'London',
+        country: 'England'
+      }
+    };
+
+  constructor() { }
+
+  ngOnInit() { }
+}
